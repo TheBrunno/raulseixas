5027a91556f2bfd55c7758056e5b9a183e7d760f
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2025-05-02 20:27:01 -0300

    upload de capas

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const multer  = require('multer')
 
-const storage = require('../config/songMulter.js');
-const songUpload = multer({ storage: storage });
+const storageSong = require('../config/songMulter.js');
+const storageCover = require('../config/coverMulter.js');
+const songUpload = multer({ storage: storageSong });
+const coverUpload = multer({ storage: storageCover });
 
 const router = express.Router();
 
 router.post('/upload/song', songUpload.single('song'), (req, res) => {
+    return res.status(200).json({ file: req.file.filename });
+});
+
+router.post('/upload/cover', coverUpload.single('cover'), (req, res) => {
     return res.status(200).json({ file: req.file.filename });
 });
 
