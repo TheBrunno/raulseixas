acb7280d6d198a1112f4600550c56c4a54bbab09
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2025-05-02 21:29:11 -0300

    primeira versao do banco de dados

---
+++ src/database/raulseixas.sql
@@ -0,0 +1,42 @@
+create database raulseixas;
+use raulseixas;
+
+create table usuario (
+  id int primary key auto_increment,
+  nome varchar(200) not null,
+  email VARCHAR(100) not null,
+  senha VARCHAR(50) not null,
+  isADM boolean,
+  foto varchar(200)
+);
+
+create table album (
+  id int primary key auto_increment,
+  nome varchar(100) not null,
+  capa varchar(200),
+  descricao varchar(1000)
+);
+
+create table musica (
+  id int,
+  fkAlbum int,
+  nome varchar(100) not null,
+  duracao time not null,
+  local varchar(200),
+  views int not null default 0,
+  
+  foreign key(fkAlbum) references album(id),
+  primary key (id, fkAlbum)
+);
+
+create table comentario (
+  id int,
+  fkUsuario int,
+  comentario varchar(1000),
+  fkMusica INT NOT NULL,
+  fkAlbum INT NOT NULL,
+  
+  foreign key(fkMusica, fkAlbum) references musica(id, fkAlbum),
+  foreign key(fkUsuario) references usuario(id),
+  primary key(id, fkUsuario)
+);
