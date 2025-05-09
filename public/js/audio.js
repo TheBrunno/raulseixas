9bb395be85dd3e3420b015ee1fb4f870fcaee159
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2025-05-08 23:04:34 -0300

    multiplas musicas tocando

--- public/js/audio.js
+++ public/js/audio.js
@@ -1,9 +1,9 @@
-const audio = document.getElementById('audio');
-const playButton = document.getElementById('play_button');
-const progress = document.getElementById('progress');
+let audio;
+let divPai;
+let progress;
 
-const currentTime = document.getElementById('current_time');
-const duration = document.getElementById('duration');
+let currentTime;
+let duration;
 
 function formatTime(time) {
     const minutes = Math.floor(time / 60);
@@ -18,23 +18,32 @@ function updateProgressBar() {
     progress.style.background = `linear-gradient(to right, #F9B631 0%, #F9B631 ${value}%, white ${value}%, white 100%)`;
 }
 
-playButton.addEventListener('click', () => {
+function play(element) {
+    divPai = element.parentElement.parentElement;
+
+    divPai.parentElement.classList.add('expanded');
+
+    audio = divPai.getElementsByTagName('audio')[0];
+    progress = divPai.querySelector('input[type="range"]')
+    currentTime = divPai.querySelector('.times span:nth-child(1)')
+    duration = divPai.querySelector('.times span:nth-child(2)')
+
     if (audio.paused) {
         audio.play();
-        playButton.innerHTML = 'pause';
+        element.innerHTML = 'pause';
     } else {
         audio.pause();
-        playButton.innerHTML = 'resume';
+        element.innerHTML = 'resume';
     }
-});
 
-audio.addEventListener('timeupdate', () => {
-    progress.value = (audio.currentTime / audio.duration) * 100;
-    currentTime.textContent = formatTime(audio.currentTime);
-    duration.textContent = formatTime(audio.duration);
-    updateProgressBar();
-});
+    audio.addEventListener('timeupdate', () => {
+        progress.value = (audio.currentTime / audio.duration) * 100;
+        currentTime.textContent = formatTime(audio.currentTime);
+        duration.textContent = formatTime(audio.duration);
+        updateProgressBar();
+    });
 
-progress.addEventListener('input', () => {
-    audio.currentTime = (progress.value / 100) * audio.duration;
-});
+    progress.addEventListener('input', () => {
+        audio.currentTime = (progress.value / 100) * audio.duration;
+    });
+};
