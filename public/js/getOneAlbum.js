6339b60d2f24b92a9966a77f56f31f34df0c608d
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2025-05-31 14:19:06 -0300

    front-end da tela de adiconar/editar album finalizada

--- public/js/getOneAlbum.js
+++ public/js/getOneAlbum.js
@@ -1,3 +1,20 @@
 function getOneAlbum(fkalbum){
-    console.log(fkalbum)
+    fetch("/album/getCards/" + fkalbum, { method: "GET" })
+    .then(result => result.json())
+    .then(json => {
+        for (let i = 0; i < json.length; i++) {
+            curiosidades.innerHTML += `
+                <div class="curiosities_card">
+                    <img src="${json[i].srcFoto}">
+                    <p>${json[i].descricao}</p>
+                </div>
+            `;
+        }
+    })
+
+    fetch('/album/getAlbumWithSongs/' + fkalbum)
+    .then(result => result.json())
+    .then(json => {
+        console.log(json)
+    })
 }
